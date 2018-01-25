d5ad361b20db78934241fbd950d53e8a670919fe
matt <mdmccoy@gmail.com>
2018-01-25 10:38:08 -0500

    Weather in yoda format.

---
+++ yoda.rb
@@ -0,0 +1,31 @@
+#Make yoda speak the weather.
+#Get the current weather from weather api
+#25.8028299,-80.2065428,
+
+require 'httparty'
+
+weather_response = HTTParty.get("https://simple-weather.p.mashape.com/weather?lat=25.8028299&lng=-80.2065428%2C",
+  headers:{
+    "X-Mashape-Key" => "3JvntCfhQemshL1jaX5BVmt1fIxVp1qDKepjsn8XR84ekQyEDy",
+    "Accept" => "text/plain"
+  })
+
+  weather =  weather_response.parsed_response.split(',')
+
+  city = weather[1].split(" ")[2]
+
+  condition = weather[1].split(" ")[0].downcase
+
+  temp = weather[0].split(" ")[0]
+
+  unit =  "celcius" if weather[0].split(" ")[1] == 'c'
+
+  pp weather =  "The temperature in #{city} is #{temp} degrees #{unit} and the weather is #{condition}."
+
+  yoda_response = response = HTTParty.get("https://yoda.p.mashape.com/yoda?sentence=#{weather.split(" ").join("+")}",
+  headers:{
+    "X-Mashape-Key" => "3JvntCfhQemshL1jaX5BVmt1fIxVp1qDKepjsn8XR84ekQyEDy",
+    "Accept" => "text/plain"
+  })
+
+  pp yoda_weather = yoda_response.parsed_response
