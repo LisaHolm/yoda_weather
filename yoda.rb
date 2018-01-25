9fe51357237823092596ca4e5f477bb5d22ebc52
matt <mdmccoy@gmail.com>
2018-01-25 17:49:07 -0500

    IT WORKS!

--- yoda.rb
+++ yoda.rb
@@ -8,56 +8,51 @@ require 'httparty'
 require 'sinatra'
 
 get '/' do
-  # your_location = location
-
   erb :location
-
-  lat_lon = [25.8028299, -80.2065428]
-  p @lat
-  p @lon
-
-  weather_response = HTTParty.get("https://simple-weather.p.mashape.com/weather?lat=#{lat_lon[0]}&lng=#{lat_lon[1]}",
-                                  headers: {
-                                    'X-Mashape-Key' => '3JvntCfhQemshL1jaX5BVmt1fIxVp1qDKepjsn8XR84ekQyEDy',
-                                    'Accept' => 'text/plain'
-                                  })
-
-  @weather = weather_response.parsed_response.split(',')
-
-  city = @weather[1].split(' ')[2]
-
-  condition = @weather[1].split(' ')[0].downcase
-
-  temp = @weather[0].split(' ')[0]
-
-  unit = if @weather[0].split(' ')[1] == 'c'
-           'celcius'
-         else
-           'farenheight'
-          end
-
-  @weather = "The temperature in #{city} is #{temp} degrees #{unit}. The weather is #{condition}."
-
-  yoda_response = HTTParty.get("https://yoda.p.mashape.com/yoda?sentence=#{@weather.split(' ').join('+')}",
-                               headers: {
-                                 'X-Mashape-Key' => '3JvntCfhQemshL1jaX5BVmt1fIxVp1qDKepjsn8XR84ekQyEDy',
-                                 'Accept' => 'text/plain'
-                               })
-
-  if yoda_response.response.code != '200'
-    # @weather
-    erb :display
-  else
-    @weather = yoda_response.parsed_response
-    erb :display
-  end
 end
 
 post '/go' do
   @lat = params[:lat]
   @lon = params[:lon]
-  # erb :index
+  erb :display
 end
+#   lat_lon = [25.8028299, -80.2065428]
+#   weather_response = HTTParty.get("https://simple-weather.p.mashape.com/weather?lat=#{lat_lon[0]}&lng=#{lat_lon[1]}",
+#                                   headers: {
+#                                     'X-Mashape-Key' => '3JvntCfhQemshL1jaX5BVmt1fIxVp1qDKepjsn8XR84ekQyEDy',
+#                                     'Accept' => 'text/plain'
+#                                   })
+#
+#   @weather = weather_response.parsed_response.split(',')
+#
+#   city = @weather[1].split(' ')[2]
+#
+#   condition = @weather[1].split(' ')[0].downcase
+#
+#   temp = @weather[0].split(' ')[0]
+#
+#   unit = if @weather[0].split(' ')[1] == 'c'
+#            'celcius'
+#          else
+#            'farenheight'
+#           end
+#
+#   @weather = "The temperature in #{city} is #{temp} degrees #{unit}. The weather is #{condition}."
+#
+#   yoda_response = HTTParty.get("https://yoda.p.mashape.com/yoda?sentence=#{@weather.split(' ').join('+')}",
+#                                headers: {
+#                                  'X-Mashape-Key' => '3JvntCfhQemshL1jaX5BVmt1fIxVp1qDKepjsn8XR84ekQyEDy',
+#                                  'Accept' => 'text/plain'
+#                                })
+#
+#   if yoda_response.response.code != '200'
+#     # @weather
+#     erb :display
+#   else
+#     @weather = yoda_response.parsed_response
+#     erb :display
+#   end
+# end
 
 # def location
 #   page = 'http://freegeoip.net/json/'
